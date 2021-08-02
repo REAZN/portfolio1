a5c2d4d425ff770efac6724bdc272f6f8ee578e4
REAZN <reasonwhygamer@gmail.com>
2021-08-03 00:31:45 +0100

    Fixed

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import {Meta, Nav, Hero, About} from "components";
+import {Meta, Nav, Hero, About, Footer} from "components";
 
 export default function Home() {
     return (
         <>
-            <Meta title={"REAZN"} description={"Hello"} image={"images/og-image.png"}/>
+            <Meta title={"REAZN"} description={"Hello"} image={"icons/og-image.png"}/>
             <Nav/>
             <Hero/>
             <About/>
+            <Footer/>
         </>
     )
 }
