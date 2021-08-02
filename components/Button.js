a75c12eefffac921c36694c09df5ac5cad3f073f
REAZN <reasonwhygamer@gmail.com>
2021-08-03 00:30:27 +0100

    Added button icon

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,13 @@
 import styles from "styles/components/Button.module.scss";
 
-export default function Button({text}) {
+export default function Button({text, icon, href}) {
     return (
-        <button className={styles["button"]}>{text}</button>
+        <a href={href} target="_blank" rel="noreferrer" className={styles["button"]} tabIndex="-1">
+            <button>
+                {text}
+                {icon}
+            </button>
+        </a>
+
     )
 }
