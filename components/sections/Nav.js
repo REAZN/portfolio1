623c15fe554662b165f5bdc9c98699c9f11b5260
reazn <github@reazn.me>
2022-06-12 11:48:47 +0100

    nav revamp

--- components/sections/Nav.js
+++ components/sections/Nav.js
@@ -1,21 +1,33 @@
 import styles from "styles/components/sections/Nav.module.scss";
+import { Link } from "react-scroll";
+import { motion } from "framer-motion";
 
 export default function Nav() {
+
+    const links = ["Home", "About", "Projects", "Experiments"];
+
     return (
         <header className={styles["nav"]}>
-            {/*<div className={styles["nav"]}>*/}
-            <a className={styles["site-name"]}>REAZN</a>
-            {/*<button className="hamburger hamburger--squeeze" id="hamburger" type="button" aria-label="Hamburger menu">*/}
-            {/*<span className="hamburger-box"/>*/}
-            {/*<span className="hamburger-inner"/>*/}
-            {/*</button>*/}
             <ul className={styles["nav-links"]}>
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Contact</a></li>
+                {links.map((link, index) => (
+                    <li key={link}>
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true }}
+                            transition={{ duration: 0.5, delay: index/10}}
+                            variants={{
+                                visible: { opacity: 1, y: 0 },
+                                hidden: { opacity: 0, y: "50%" }
+                            }}
+                        >
+                            <Link to={`section-${link.toLowerCase()}`} offset={-100} smooth={true} duration={700}>
+                                <span className={styles["link-numbers"]}>0{index + 1}.</span> {link}
+                            </Link>
+                        </motion.div>
+                    </li>
+                ))}
             </ul>
-            {/*</div>*/}
         </header>
     )
 }
