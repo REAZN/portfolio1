dcd6338ec8f901e06abe355d9dc512692a6b8066
reazn <github@reazn.me>
2022-06-12 11:47:24 +0100

    cleaned up code + animations

--- components/sections/About.js
+++ components/sections/About.js
@@ -1,31 +1,61 @@
 import styles from "styles/components/sections/About.module.scss";
 import Script from "next/script";
 import { Button, Title, HtmlTag, Sphere, LinkIcon } from "components";
+import { motion } from "framer-motion";
+import config from "config";
 
 export default function About() {
 
     return (
-        <section className={styles["about"]}>
+        <section id="section-about" className={styles["about"]}>
             <Script type="text/javascript" src="/tagcanvas.min.js" strategy="beforeInteractive" />
             <div className={styles["content"]}>
                 <div className={styles["left"]}>
                     <Title text="About Me" />
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+                        transition={{ duration: 0.5}}
+                        variants={{
+                            visible: { opacity: 1, y: 0 },
+                            hidden: { opacity: 0, y: "5%" }
+                        }}
+                    >
                     <HtmlTag tag="p">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ducimus error facilis
-                            necessitatibus porro tempore? Adipisci dolorem iste modi natus praesentium sed totam.
-                            Consectetur exercitationem iure reprehenderit ullam. Animi, ipsum. Lorem ipsum dolor sit
-                            amet,
-                            consectetur adipisicing elit. Architecto atque ipsa nam natus non numquam quam quisquam.
+                            {config.about}
                         </p>
                     </HtmlTag>
-                    <Button icon={<LinkIcon fill="var(--accent)" />} href={"/cv"} style={{ marginTop: "1em" }}>
-                        Curriculum Vitae
-                    </Button>
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+                        transition={{ duration: 0.5, delay: 0.2}}
+                        variants={{
+                            visible: { opacity: 1, y: 0 },
+                            hidden: { opacity: 0, y: "5%" }
+                        }}
+                    >
+                        <Button icon={<LinkIcon fill="var(--accent)" />} href={"/cv"} style={{ marginTop: "1em" }}>
+                            Curriculum Vitae
+                        </Button>
+                    </motion.div>
+
+                    </motion.div>
                 </div>
-                <div className={styles["right"]}>
+                <motion.div className={styles["right"]}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{ duration: 0.5}}
+                    variants={{
+                        visible: { opacity: 1, y: 0 },
+                        hidden: { opacity: 0, y: "5%" }
+                    }}
+                >
                     <Sphere style={{ width: "100%", height: "100%" }} />
-                </div>
+                </motion.div>
             </div>
         </section>
     )
